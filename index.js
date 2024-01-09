97a62c05bbb0fc291b54e42db6a0da85a37de250
SuliucMihaiLucian <114436167+luck1ano@users.noreply.github.com>
2024-01-09 05:07:19 +0200

    Continuare proiect

--- index.js
+++ index.js
@@ -1,8 +1,36 @@
 const inputFieldEl = document.getElementById("input-field")
 const addButtonEl = document.getElementById("add-button")
+const shoppingListEl = document.getElementById("shopping-list")
+
+import {initializeApp} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
+import {getDatabase, ref, push, onValue} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+
+const appSettings = {
+    databaseURL: "https://realtime-database-8f1da-default-rtdb.europe-west1.firebasedatabase.app/"
+}
+
+const app = initializeApp(appSettings)
+const database = getDatabase(app)
+const shoppingListinDB = ref(database, "shoppingList")
 
 addButtonEl.addEventListener("click", function() {
     let inputValue = inputFieldEl.value
-    
-    console.log(inputValue)
+    push(shoppingListinDB, inputValue)
+    clearInputFieldEl()
+    appendItemToShoppingListEl(inputValue)
 })
+
+onValue (shoppingListinDB, function(snapshot) {
+    let itemsArray = Object.values(snapshot.val())
+    for(let i = 0; i < itemsArray.length; i++) {
+        appendItemToShoppingListEl(itemsArray[i])
+    }
+})
+
+function clearInputFieldEl() {
+    inputFieldEl.value = ""
+}
+
+function appendItemToShoppingListEl(itemValue) {
+    shoppingListEl.innerHTML += `<li>${itemValue}</li>`
+}
